dabd8495ead2377a31905523edcb3c129449b038
Leonardo0908 <leonardo.elias0908@gmail.com>
2022-03-21 17:14:36 -0300

    React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo"> Home </h1>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrXfbdfhPzEnJO8EDTW1rwEvsle5fKAduSJQ&usqp=CAU" alt="imagem Local" className="img"/>
+        </>
+    );
+}
+
+export default Home;
