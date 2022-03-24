8ae0520fc5c2bd22bbfaaf0e31fb2b9a443fdd7f
Leonardo0908 <leonardo.elias0908@gmail.com>
2022-03-24 14:23:57 -0300

    sistema de rotas criado

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -1,27 +1,38 @@
 import React from "react";
-import InstagramIcon  from "@material-ui/icons/Instagram";
-import FacebookIcon  from "@material-ui/icons/Facebook";
-import LinkedInIcon  from "@material-ui/icons/LinkedIn";
-import {Grid, Box, Typography } from "@material-ui/core";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import { Grid, Box, Typography } from "@material-ui/core";
+import "./Footer.css";
 
-function Footer(){
-    return(
+function Footer() {
+    return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3f51b5", height: "120px" }}>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center" >
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais</Typography>
+                    <Box className='box1'>
+                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                            <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
                         </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center" >
-                            <a href="http://facebook.com.br/" target="blank" >
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                            <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                                <FacebookIcon className='redes' />
                             </a>
-                            <a href="http://instagram.com.br/">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} target="blank" />
+                            <a href="https://www.instagram.com/generationbrasil/" target="_blank">
+                                <InstagramIcon className='redes' />
                             </a>
-                            <a href="http://linkedin.com.br/">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} target="blank" />
+                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
+                                <LinkedInIcon className='redes' />
+                            </a>
+                        </Box>
+                    </Box>
+                    <Box className='box2'>
+                        <Box paddingTop={1}>
+                            <Typography variant="subtitle2" align="center" gutterBottom className='textos' >© 2020 Copyright:</Typography>
+                        </Box>
+                        <Box>
+                            <a target="_blank" href="https://brasil.generation.org">
+                                <Typography variant="subtitle2" gutterBottom className='textos' align="center">brasil.generation.org</Typography>
                             </a>
                         </Box>
                     </Box>
