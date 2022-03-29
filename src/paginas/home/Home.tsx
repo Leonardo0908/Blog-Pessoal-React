8ab1a3a9326729733f7718f40d0b2046028a72e2
Leonardo0908 <leonardo.elias0908@gmail.com>
2022-03-29 17:13:39 -0300

    implementando o GetAll da model de Tema e Postagendo

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Typography, Box, Grid, Button} from '@material-ui/core';
+import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
 import './Home.css';
+
 
 function Home() {
     return (
@@ -21,6 +23,7 @@ function Home() {
                     <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
                 </Grid>
                 <Grid xs={12} className='postagens'>
+                    <TabPostagem />
                 </Grid>
             </Grid>
         </>
