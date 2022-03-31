b79e8452bccd42f0be20c100fe73dea94e987e75
Leonardo0908 <leonardo.elias0908@gmail.com>
2022-03-31 17:07:10 -0300

    implementando o Post Put e Delete da model de Tema e Postagem

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import {AppBar, Toolbar, Typography, Box} from "@material-ui/core"; 
 import {Link} from "react-router-dom";
+import {useHistory} from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
 import "./Navbar.css";
 
 function Navbar(){
+    const [token, setToken] = useLocalStorage("token");
+    let history = useHistory();
+
+    function goLogout(){
+        setToken("")
+        alert("Usuário deslogado")
+        history.push('/login')
+    }
     return(
         <>
         
          <AppBar position="static" className="back">
              <Toolbar variant="dense">
                  <Box className="cursor">
-
                      <Typography variant="h5" color="inherit">
                     BlogPessoal
                      </Typography>
@@ -19,37 +28,42 @@ function Navbar(){
                  <Box display="flex" justifyContent="start">
 
                     <Link to="/home" className="text-decorator-none">
-                     <Box mx={1} className="cursor">
-                         <Typography variant="h6" color="inherit">
-                             Home
-                         </Typography>
-                     </Box>
+                        <Box mx={1} className="cursor">
+                            <Typography variant="h6" color="inherit">
+                                Home
+                            </Typography>
+                        </Box>
                     </Link>
 
-                     <Box mx={1} className="cursor">
-                        <Typography variant="h6" color="inherit" >
-                            Postagens
-                        </Typography>
-                     </Box>
-                     <Box mx={1} className="cursor">
-                        <Typography variant="h6" color="inherit" >
-                            Temas
-                        </Typography>
-                     </Box>
+                    <Link to="/post" className="text-decorator-none">
+                        <Box mx={1} className="cursor">
+                            <Typography variant="h6" color="inherit" >
+                                Postagens
+                            </Typography>
+                        </Box>
+                     </Link>
 
-                     <Box mx={1} className="cursor">
-                        <Typography variant="h6" color="inherit" >
-                            Cadastrar Temas
-                        </Typography>
-                     </Box>
-                     
-                     <Link to="/login" className="text-decorator-none">
-                    <Box mx={1} style={{ cursor: "pointer", color: "white" }}>
+                     <Link to="/temas" className="text-decorator-none">
+                        <Box mx={1} className="cursor">
+                            <Typography variant="h6" color="inherit" >
+                                Temas
+                            </Typography>
+                        </Box>
+                     </Link>
+
+                    <Link to="/formularioTema" className="text-decorator-none">
+                        <Box mx={1} className="cursor">
+                            <Typography variant="h6" color="inherit" >
+                                Cadastrar Temas
+                            </Typography>
+                        </Box>
+                     </Link>
+
+                        <Box mx={1} onClick={goLogout} style={{ cursor: "pointer", color: "white" }}>
                             <Typography variant="h6" color="inherit" >
                                 Logout
                             </Typography>
                         </Box>
-                     </Link>
                  </Box>
              </Toolbar>
          </AppBar>
